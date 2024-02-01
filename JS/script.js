ab0e1af9659bc4ed15f97f614b10a46241d2f353
Pavel Rivera <pavelgtr@gmail.com>
2024-02-01 16:27:02 -0700

    complete slider functionality, sizes too large

--- JS/script.js
+++ JS/script.js
@@ -1,1 +1,30 @@
 
+let swiperCards = new Swiper('.card__content', {
+  loop: false,
+  spaceBetween: 32,
+  slidesPerView: 3, 
+  grabCursor: true,
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true, 
+    dynamicBullets: true,
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+  breakpoints: {
+    512:{
+      slidesPerView: 1
+    },
+    600:{
+      slidesPerView: 2
+    },
+    968:{
+      slidesPerView: 3
+    },
+  },
+});
